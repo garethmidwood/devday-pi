7f68dcda1f7a05aff56b7d75697aaf5d028b13d8
Gareth Midwood <gareth.midwood@creode.co.uk>
2016-10-03 23:31:57 +0100

    Add more appropriate sounds

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ console.log(config.server.host + ":" + config.server.port);
 
 var ddpclient = new ddp({ host: config.server.host, port: config.server.port, autoReconnect : true });
 
+var crowdSoundsOn = false;
+
 ddpclient.connect(function(error) {
     if (error) {
         console.log('ah crap');
@@ -31,16 +33,36 @@ ddpclient.connect(function(error) {
     observer.added = function(_id) {
         console.log('added ' + _id);
         if (_id === "gameWinner") {
-            player.play('./samples/cookie.mp3');
+            player.play('./samples/game-over-yeah.mp3');
         }
     };
 
     observer.changed = function(_id, oldFields, clearedFields, newFields) {
         console.log('changed ' + _id);
+        if (_id === 'game') {
+            if (newFields.inProgress === true) {
+                console.log('starting crowd sounds');
+                crowdSoundsOn = true;
+                crowdSound();
+            } else {
+                console.log('ending crowd sounds');
+                crowdSoundsOn = false;
+            }
+        }
     };
 
     observer.removed = function(_id, oldValue) {
         console.log('removed ' + _id);
     }
 });
+
+function crowdSound() {
+    player.play('./samples/crowd.mp3', function() {
+        console.log('crowd done');
+        if (crowdSoundsOn) {
+            console.log('repeating crowd sounds');
+            crowdSound();
+        }
+    });
+}
 
